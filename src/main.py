ef1cbab92d72061e5d5fac6b902f753a7b69c1e6
Andrew Gunter <andrewdavidgunter@gmail.com>
2021-02-20 16:47:55 -0800

    Fixed up documentation

--- src/main.py
+++ src/main.py
@@ -2,27 +2,34 @@ import cProfile
 import pstats
 import sim_anneal
 
-# "apex1.txt",
-#FILE_NAMES = ["apex4.txt", "C880.txt", "cm138a.txt", "cm150a.txt", "cm151a.txt",
- #             "cm162a.txt", "cps.txt", "e64.txt", "paira.txt", "pairb.txt"]
-FILE_NAMES = ["cm162a.txt"]
-COOLING_FACTORS = [0.8]
-INITIAL_TEMP_FACTORS = [10]
-MOVES_PER_TEMP_FACTORS = [75]
+# Experimental grid search parameters
+FILE_NAMES = ["alu2.txt", "apex1.txt", "apex4.txt", "C880.txt", "cm138a.txt", "cm150a.txt", "cm151a.txt",
+              "cm162a.txt", "cps.txt", "e64.txt", "paira.txt", "pairb.txt"]
+COOLING_FACTORS = [0.8, 0.85, 0.9]
+INITIAL_TEMP_FACTORS = [10, 20, 30]
+MOVES_PER_TEMP_FACTORS = [25, 50, 75]
+
 
 def main():
-    #profiler = cProfile.Profile()
-    #profiler.enable()
-    for file_name in FILE_NAMES:
-        for cooling_factor in COOLING_FACTORS:
-            for initial_temp_factor in INITIAL_TEMP_FACTORS:
-                for moves_per_temp_factor in MOVES_PER_TEMP_FACTORS:
-                    #sim_anneal.quick_anneal(file_name, cooling_factor, initial_temp_factor, moves_per_temp_factor)
-                    sim_anneal.anneal()
-    #profiler.disable()
-    #stats = pstats.Stats(profiler).sort_stats('cumtime')
-    #stats.print_stats()
-    print("end")
+    """
+    Main function for running annealing experiments
+    """
+    experimental_mode = False
+
+    if experimental_mode:
+        profiler = cProfile.Profile()
+        profiler.enable()
+        for file_name in FILE_NAMES:
+            for cooling_factor in COOLING_FACTORS:
+                for initial_temp_factor in INITIAL_TEMP_FACTORS:
+                    for moves_per_temp_factor in MOVES_PER_TEMP_FACTORS:
+                        sim_anneal.quick_anneal(file_name, cooling_factor, initial_temp_factor, moves_per_temp_factor)
+        profiler.disable()
+        stats = pstats.Stats(profiler).sort_stats('cumtime')
+        stats.print_stats()
+        print("end")
+    else:
+        sim_anneal.anneal()
 
 
 if __name__ == "__main__":
